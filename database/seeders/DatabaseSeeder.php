c447456cdffa1520b093ab59fd6b451e62e425b5
Admin Nekmese <admin@nekmese.desa>
2026-07-30 21:52:54 +0800

    DatabaseSeeder auto-copy SQLite data after seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,14 +5,12 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call([
@@ -21,5 +19,9 @@ class DatabaseSeeder extends Seeder
             SettingSeeder::class,
             SidebarMenuSeeder::class,
         ]);
+
+        if (file_exists(database_path('database.sqlite'))) {
+            Artisan::call('db:copy-sqlite-to-mysql');
+        }
     }
 }
